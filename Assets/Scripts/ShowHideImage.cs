fc2e0d5724371d3495c4687303c00bbc0e2500b3
Acortez5 <acortez5@email.cpcc.edu>
2024-03-22 21:29:39 -0400

    Made it easier to tell buttons are toggle
    Changed GuideScene and ShowHideImage.cs so when the buttons are pressed, the background changes color to make it clearer to the user that the buttons are toggle.

--- Assets/Scripts/ShowHideImage.cs
+++ Assets/Scripts/ShowHideImage.cs
@@ -7,6 +7,43 @@ public class ShowHideImage : MonoBehaviour
 {
     public Image imageToToggle;
 
+    private Button button;
+
+    private bool isPressed = false;
+
+    public Color pressedColor;
+    public Color unpressedColor;
+
+    private void Start()
+    {
+        // Get the Button component attached to this GameObject
+        button = GetComponent<Button>();
+
+        // Set the initial color of the button
+        button.image.color = unpressedColor;
+
+        // Add a listener to the button's onClick event
+        button.onClick.AddListener(ChangeColor);
+    }
+
+    
+
+    void ChangeColor()
+    {
+        // Toggle the state of the button
+        isPressed = !isPressed;
+
+        // Change the color based on the button state
+        if (isPressed)
+        {
+            button.image.color = pressedColor;
+        }
+        else
+        {
+            button.image.color = unpressedColor;
+        }
+    }
+
     public void ToggleImageVisibility()
     {
         imageToToggle.gameObject.SetActive(!imageToToggle.gameObject.activeSelf);
